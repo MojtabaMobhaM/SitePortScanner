c90f7200547cb80ab95d30032afa1ae92676363c
MojtabaMobhaM <kazemimojtaba@live.com>
2016-12-11 18:04:39 +0330

    Update SitePortScanner.py

--- SitePortScanner.py
+++ SitePortScanner.py
@@ -10,7 +10,7 @@ target = raw_input("Enter Address : ")
 if ('http' in target):
     print ("Start")
     print ("take long time Scaning 1 - 10000 Port...")
-    for i in range(1,10000):
+    for i in range(80,10000):
         site = target + ":" + str(i)
         try:
             r = requests.get(site,timeout=1)
